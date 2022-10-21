2ba222668a58460d5c40f6cd2a37b46106275fe1
collinradichel <radichel.collin@gmail.com>
2022-10-20 21:28:19 -0500

    building out the skeleton of the project

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,1 +1,19 @@
-<div>Hello World</div>
+// import { useSelector } from 'react-redux';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" exact></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './Components/App/App';
+import project from './modules/project.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const projectReducer = (state = project, action) => {
+  return state;
+};
+
+const store = createStore(
+  combineReducers({
+    projectReducer
+  })
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
